44ab3883ed183d5cc873813802d4013437d3164d
sscodez <ssameershah1200@gmail.com>
2023-08-04 02:39:19 +0500

    Added

--- app.js
+++ app.js
@@ -6,26 +6,14 @@ const {
     getFirestore,
     collection,
     getDocs,
+    getDoc,
+    updateDoc,
     addDoc,where,query,doc
   } = require('firebase/firestore/lite');
 const server = coap.createServer();
 
-const bikeSchema = {
-    bikeId: '', // The unique ID for the bike
-    bikeModel: "", // The bike model
-    bikeName: "", // The bike name
-    frontBrake: false, // Whether the bike has a front brake or not
-    rearBrake: false, // Whether the bike has a rear brake or not
-    gear: 0, // The bike gear
-    latitude: 0.0, // The bike's latitude
-    longitude: 0.0, // The bike's longitude
-    cyclistId: null, // The ID of the cyclist who rented the bike (null if not rented)
-    isRented: false, // Whether the bike is currently rented or not
-  };
 
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+
 function handleAddBikes(req, res) {
     if (req.method === 'POST' && req.url === '/addBikes') {
  
@@ -46,7 +34,7 @@ function handleAddBikes(req, res) {
         gear: 0, 
         latitude: 0.0, 
         longitude: 0.0, 
-        cyclistId: null, 
+        cyclistId: 0, 
         isRented: false, 
     }
     
@@ -92,13 +80,32 @@ async function data(req,res){
 
     }
     
+    async function data1(req,res){
+      const collectionName = 'Bikes';
+          console.log('getfunction')
+              // Use the "where" function to create a query that filters documents where "rented" is false
+              const q=  query(collection(db, collectionName), where('isRented', '==', true));
+              const querySnapshot = await getDocs(q);
+              let BikesArr=[];
+      
+              // Loop through the querySnapshot and display the available bikes
+              await Promise.all(
+                  querySnapshot.docs.map(async (doc) => {
+                    const bikeData = doc.data();
+                    BikesArr.push(bikeData.bikeId,bikeData.bikeName);
+                  })
+                );
+  
+          return BikesArr
+  
+      }
+      
 
 
     async function handleAvailableBikes(req, res) {
       if (req.method === 'GET' && req.url === '/availableBikes') {
           try {
               const b = await data();
-  
               res.setHeader('Content-Type', 'application/json');
               res.end(JSON.stringify(b));
           } catch (error) {
@@ -118,34 +125,40 @@ async function data(req,res){
         try {
             const collectionName = 'Bikes';
             res.setOption('Content-Format','application/json');
+
             const bikeData = JSON.parse(req.payload.toString());
             console.log(bikeData)
-            // Validate the incoming data against )the schema
-            // ... (your validation code here)
+            const bikeDb = collection(db, collectionName);
 
-            const bikeRef = doc(db, collectionName, bikeData.bikeId);
-            const bikeDoc = await getDoc(bikeRef);
-            console.log('bikeDoc',bikeDoc)
-
-            if (bikeDoc.exists() && bikeDoc.data().rented === false && bikeDoc.data().cyclistId === null) {
-                await updateDoc(bikeRef, {
-                    cyclistId: bikeData.cyclistId,
-                    rented: true,
-                });
-
+            // Use a query to find the bike document with the given bikeId and isRented status
+            const q = query(bikeDb, where('bikeId', '==', bikeData.bikeId), where('isRented', '==', false));
+            const querySnapshot = await getDocs(q);
+      
+            // Check if the document exists
+            if (!querySnapshot.empty) {
+              const bikeDoc = querySnapshot.docs[0];
+              const bikeRef = doc(bikeDb, bikeDoc.id);
+      
+              // Update the bike document with the cyclistId and isRented status
+              await updateDoc(bikeRef, {
+                cyclistId: bikeData.cyclistId,
+                isRented: true,
+              });
                 res.setOption('Content-Format', 'application/json');
                 res.code = '2.05';
                 res.end(JSON.stringify(`Bike with ID ${bikeData.bikeId} updated successfully.`));
                 console.log(`Bike with ID ${bikeData.bikeId} updated successfully.`);
-            } else {
-                console.log('The bike is not available for renting.');
-                res.code = '4.04'; // Not Found
-                res.setOption('Content-Format','application/json');
-                res.end(JSON.stringify('The bike is not available for renting.'));
-            }
-        } catch (error) {
+          } 
+          else {
+            console.log('The bike is not available for renting.');
+            res.code = '4.04'; // Not Found
+            res.end(JSON.stringify('The bike is not available for renting.'));
+          }
+        }
+         catch (error) {
             console.error('Error updating bike data: ', error);
             res.code = '5.00'; // Internal Server Error
+            res.setOption('Content-Format', 'application/json');
             res.end(JSON.stringify('Something went wrong'));
         }
     } else {
@@ -153,37 +166,64 @@ async function data(req,res){
         res.end(JSON.stringify('Invalid issue'));
     }
 }
+async function handleEndRentBikes(req, res) {
+  if (req.method === 'POST' && req.url === '/rentBikes') {
+      try {
+          const collectionName = 'Bikes';
+          res.setOption('Content-Format','application/json');
 
+          const bikeData = JSON.parse(req.payload.toString());
+          console.log(bikeData)
+          const bikeDb = collection(db, collectionName);
+
+          // Use a query to find the bike document with the given bikeId and isRented status
+          const q = query(bikeDb, where('bikeId', '==', bikeData.bikeId), where('isRented', '==', true));
+          const querySnapshot = await getDocs(q);
+    
+          // Check if the document exists
+          if (!querySnapshot.empty) {
+            const bikeDoc = querySnapshot.docs[0];
+            const bikeRef = doc(bikeDb, bikeDoc.id);
+    
+            // Update the bike document with the cyclistId and isRented status
+            await updateDoc(bikeRef, {
+              cyclistId: 0,
+              isRented: false,
+            });
+              res.setOption('Content-Format', 'application/json');
+              res.code = '2.05';
+              res.end(JSON.stringify(`End Rente Bike with ID ${bikeData.bikeId} updated successfully.`));
+              console.log(`Bike with ID ${bikeData.bikeId} updated successfully.`);
+        } 
+        else {
+          console.log('The bike ride is already ended.');
+          res.code = '4.04'; // Not Found
+          res.end(JSON.stringify('The bike  is not on renting'));
+        }
+      }
+       catch (error) {
+          console.error('Error updating bike data: ', error);
+          res.code = '5.00'; // Internal Server Error
+          res.setOption('Content-Format', 'application/json');
+          res.end(JSON.stringify('Something went wrong'));
+      }
+  } else {
+      res.code = '4.04'; // Not Found
+      res.end(JSON.stringify('Invalid issue'));
+  }
+}
   async  function handleNonAvailableBikes(req, res) {
     if (req.method === 'GET' && req.url === '/nonavailableBikes') {
  
-        try {
-            const collectionName = 'Bikes';
-        
-            const q=  query(collection(db, collectionName), where('isRented', '==', true));
-            const querySnapshot = await getDocs(q);
-         
-            if(querySnapshot.empty){
-                console.log('No data found');
-                res.setOption('Content-Format','application/json');
-                res.code='2.05';
-                res.end (JSON.stringify('No data found'));
-                return ;
-            }
-
-            // Loop through the querySnapshot and display the non-available bikes
-            querySnapshot.docs.forEach((doc) => {
-              const bikeData = doc.data();
-              console.log(`Bike ID: ${doc.id}`);
-              console.log('Bike Details:', bikeData);
-              res.setOption('Content-Format','application/json');
-              res.code='2.05';
-              res.end (JSON.stringify(bikeData));
-            });
-          } catch (error) {
-            console.error('Error retrieving available bikes: ', error);
-          }
+      try {
+        const b = await data1();
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(b));
+    } catch (error) {
+        res.statusCode = 500; // Internal Server Error
+        res.end(JSON.stringify('Something went wrong'));
     }
+  }
     else{
         res.code = '4.04'; // Not Found
         res.end(JSON.stringify('Invalid issue'));
@@ -238,7 +278,7 @@ function handleRootRequest(req, res) {
     console.log('GET request received on root route');
     res.setOption('Content-Format','application/json');
     res.code='2.05';
-    res.end (JSON.stringify('Hey'));
+    res.end (JSON.stringify('Hey , Welcome to Bike Sharing System '));
   } else if (req.method === 'POST') {
     console.log('POST request received on root route');
     res.end('Received a POST request!');
